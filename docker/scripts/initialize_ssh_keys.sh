757147644cefa67c8c86eee10b2cb4c66a273148
Muhammad Faisal <faisalazam_1616@hotmail.com>
2024-12-12 11:41:27 +1100

    Added comments to fix the scripts if error says that script is not found.

--- docker/scripts/initialize_ssh_keys.sh
+++ docker/scripts/initialize_ssh_keys.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
+# If you face script not found error even though the script does exist, then it'll more likely be due to the fact that
+# the script has CRLF line terminators (Windows-style). Run the following command to convert CRLF to LF to fix it:
+# dos2unix scripts/initialize_ssh_keys.sh
+# You can run the following to check the line terminators:
+# file scripts/initialize_ssh_keys.sh
+# If the output contains something like below, then it'd mean that it needs fixing to run on unix systems:
+# ASCII text executable, with CRLF line terminators
+
 ROOT_USER="root"
 SSH_DIR="/root/.ssh"
 AUTHORIZED_KEYS="${SSH_DIR}/authorized_keys"
