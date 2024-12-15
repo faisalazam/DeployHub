3a166581ce98221fc5d06a7b985b54f5f66e8f5c
Muhammad Faisal <faisalazam_1616@hotmail.com>
2024-12-15 18:25:15 +1100

    Extracted some variables.

--- vagrant/scripts/setup_winrm_https.ps1
+++ vagrant/scripts/setup_winrm_https.ps1
@@ -1,9 +1,11 @@
-# Define paths for certificate and password
+# Define configurable parameters
+$WinRMGuestPort = 5986
 $CertCN = "localhost"
 $certPath = "C:\certs"
 $RoleName = "Administrator"
 $certPass = "YourCertPassword"
 $certFile = "$certPath\winrm-cert.pfx"
+$FirewallRuleName = "WinRM-HTTPS"
 
 # Check if script is running as Administrator
 Write-Host "[INFO] Checking if script is running as $RoleName..."
@@ -34,7 +36,8 @@ if (-not (Test-Path -Path $certFile)) {
 
 # Install the certificate in the Windows certificate store
 Write-Host "[INFO] Installing certificate in the Windows certificate store..."
-Import-PfxCertificate -FilePath $certFile -CertStoreLocation "Cert:\LocalMachine\My" -Password (ConvertTo-SecureString -String $certPass -AsPlainText -Force)
+$secureCertPass = ConvertTo-SecureString -String $certPass -AsPlainText -Force
+Import-PfxCertificate -FilePath $certFile -CertStoreLocation "Cert:\LocalMachine\My" -Password $secureCertPass
 
 # Ensure WinRM service is running
 if ((Get-Service -Name WinRM).Status -ne 'Running') {
@@ -51,7 +54,8 @@ if ($winrmConfig -match "Enabled\s*=\s*true") {
 }
 
 # Retrieve the thumbprint of the certificate
-$thumbprint = (Get-ChildItem -Path Cert:\LocalMachine\My | Where-Object { $_.Subject -match "CN=$CertCN" }).Thumbprint
+$thumbprint = (Get-ChildItem -Path Cert:\LocalMachine\My `
+                    | Where-Object { $_.Subject -match "CN=$CertCN" }).Thumbprint
 
 # Set trusted hosts to allow remote connections
 Set-Item -Path WSMan:\localhost\Client\TrustedHosts -Value "*" -Force
@@ -67,7 +71,7 @@ Set-Item -Path "$winrmPath\CertificateThumbprint" -Value $thumbprint
 
 # Configure the WinRM listener for HTTPS
 Write-Host "[INFO] Configuring WinRM listener for HTTPS..."
-$listenerConfig = '@{Hostname="";CertificateThumbprint="' + $thumbprint + '";Port="5986"}'
+$listenerConfig = '@{Hostname="";CertificateThumbprint="' + $thumbprint + '";Port="' + $WinRMGuestPort + '"}'
 $command = "winrm create winrm/config/Listener?Address=*+Transport=HTTPS '$listenerConfig'"
 Invoke-Expression $command
 
@@ -75,9 +79,10 @@ Write-Host "[INFO] Validating the Listener..."
 winrm enumerate winrm/config/Listener
 
 # Check and add firewall rule for HTTPS
-if (-not (Get-NetFirewallRule -Name "WinRM-HTTPS" -ErrorAction SilentlyContinue)) {
+if (-not (Get-NetFirewallRule -Name $FirewallRuleName -ErrorAction SilentlyContinue)) {
     Write-Host "[INFO] Adding firewall rule for WinRM over HTTPS..."
-    New-NetFirewallRule -DisplayName "Allow WinRM over HTTPS" -Name "WinRM-HTTPS" -Protocol TCP -LocalPort 5986 -Action Allow
+    New-NetFirewallRule -DisplayName "Allow WinRM over HTTPS" -Name $FirewallRuleName `
+                        -Protocol TCP -LocalPort $WinRMGuestPort -Action Allow
 } else {
     Write-Host "[INFO] Firewall rule for WinRM over HTTPS already exists."
 }
