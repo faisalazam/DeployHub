b18d7f925f334cb1158b7053cd85ba27c934178e
Muhammad Faisal <faisalazam_1616@hotmail.com>
2025-01-02 16:38:54 +1100

    Corrected the condition to check if the secrets engine is already enabled or not.

--- docker/hashicorp_vault/scripts/vault_startup.sh
+++ docker/hashicorp_vault/scripts/vault_startup.sh
@@ -63,7 +63,7 @@ if [ "$SERVER_MODE" = "prod" ]; then
 
   echo "Enabling Secrets Engine at path=${SECRETS_PATH}..."
   # Check if the secrets engine is already enabled at the specified path
-  if vault secrets list | grep -q "${SECRETS_PATH}"; then
+  if vault read "sys/mounts/${SECRETS_PATH}" > /dev/null 2>&1; then
     echo "Secrets engine at path=${SECRETS_PATH} is already enabled. Skipping..."
   else
     # Enable the secrets engine if it is not already enabled
