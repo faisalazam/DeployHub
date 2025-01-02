e342b9b333171a65030c349336281c80881f9a42
Muhammad Faisal <faisalazam_1616@hotmail.com>
2025-01-02 11:40:18 +1100

    Update healthcheck to target Vault's internal port 8200 instead of 8300

--- docker/hashicorp_vault/scripts/vault_startup.sh
+++ docker/hashicorp_vault/scripts/vault_startup.sh
@@ -20,7 +20,7 @@ fi
 # Wait for Vault to be ready
 echo "Waiting for Vault to be ready..."
 until printf 'GET /v1/sys/health HTTP/1.1\r\nHost: localhost\r\n\r\n' \
-              | nc -w 5 127.0.0.1 8300 \
+              | nc -w 5 127.0.0.1 8200 \
               | grep '200 OK' > /dev/null 2>&1; do
   # Check if we've exceeded the maximum number of retries
   if [ $RETRY_COUNT -ge $MAX_RETRIES ]; then
