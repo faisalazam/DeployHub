267561b2c2651b430b2e4ddeebae8d3e954db54d
Muhammad Faisal <faisalazam_1616@hotmail.com>
2025-01-03 09:55:03 +1100

    Improved logging and error handling in common.sh script.

--- docker/hashicorp_vault/scripts/common.sh
+++ docker/hashicorp_vault/scripts/common.sh
@@ -13,38 +13,49 @@ log() {
 # Wait for Vault to be ready
 check_vault_status() {
   STATUS_TEXT=$1
+  if ! command -v nc > /dev/null 2>&1; then
+    log "Error: 'nc' (Netcat) command not found. Please install it to proceed."
+    exit 1
+  fi
   until printf 'GET /v1/sys/health HTTP/1.1\r\nHost: localhost\r\n\r\n' \
                 | nc -w 5 127.0.0.1 8200 \
                 | grep -E "${STATUS_TEXT}" > /dev/null 2>&1; do
     # Check if we've exceeded the maximum number of retries
     if [ $RETRY_COUNT -ge $MAX_RETRIES ]; then
-      echo "Vault readiness check timed out after $MAX_RETRIES retries."
+      log "Vault readiness check timed out after $MAX_RETRIES retries."
       exit 1
     fi
 
-    echo "Vault is not ready yet, retrying..."
+    log "Vault is not ready yet, retrying... (${RETRY_COUNT}/${MAX_RETRIES})"
     RETRY_COUNT=$((RETRY_COUNT + 1))
     sleep $RETRY_INTERVAL
   done
+  log "Vault is ready."
 }
 
 login_with_token() {
   LINE_NUMBER=$1
-  if [ -f "$KEYS_FILE" ]; then
-    LOGIN_TOKEN=$(sed -n "$LINE_NUMBER" "$KEYS_FILE" | awk '{print $NF}')
-    if [ -z "$LOGIN_TOKEN" ]; then
-      echo "Error: The token is empty. Exiting..."
-      exit 1
-    fi
 
-    echo "Logging in with the required token..."
-    if ! vault login "$LOGIN_TOKEN" > /dev/null 2>&1; then
-      echo "Error: Failed to log in with the token. Exiting..."
-      exit 1
-    fi
-    echo "Successfully logged in with the token."
-  else
-    echo "Login token file is missing, cannot login."
+  if [ ! -f "$KEYS_FILE" ]; then
+    log "Error: Key file '$KEYS_FILE' not found. Exiting..."
     exit 1
   fi
+
+  if [ ! -s "$KEYS_FILE" ]; then
+    log "Error: Key file '$KEYS_FILE' is empty. Exiting..."
+    exit 1
+  fi
+
+  LOGIN_TOKEN=$(sed -n "$LINE_NUMBER" "$KEYS_FILE" | awk '{print $NF}')
+  if [ -z "$LOGIN_TOKEN" ]; then
+    log "Error: Token at line $LINE_NUMBER is empty. Exiting..."
+    exit 1
+  fi
+
+  log "Logging in with the required token..."
+  if ! vault login "$LOGIN_TOKEN" > /dev/null 2>&1; then
+    log "Error: Failed to log in with the token. Exiting..."
+    exit 1
+  fi
+  log "Successfully logged in with the token."
 }
