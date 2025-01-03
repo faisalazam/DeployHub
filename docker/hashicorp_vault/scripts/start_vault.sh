f4497b7166fbdeb2a9ed8a80691c321687c40b13
Muhammad Faisal <faisalazam_1616@hotmail.com>
2025-01-03 16:39:23 +1100

    Extracted server startup to separate script.

---
+++ docker/hashicorp_vault/scripts/start_vault.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+. /opt/vault/common.sh
+
+terminate_vault() {
+  if [ -n "$VAULT_PID" ]; then
+    log "Terminating Vault server..."
+    kill $VAULT_PID
+    wait $VAULT_PID 2>/dev/null
+    log "Vault server terminated."
+  else
+    log "Vault server was not running."
+  fi
+}
+
+trap terminate_vault INT TERM EXIT
+
+# Start Vault server in the background
+log "Starting Vault server..."
+if [ "$SERVER_MODE" = "prod" ]; then
+  export VAULT_EXTERNAL_PORT=8200
+  sed "s|\${VAULT_EXTERNAL_PORT}|$VAULT_EXTERNAL_PORT|g" /vault/config/vault_config.hcl > /vault/config/vault_config_substituted.hcl
+  vault server -config=/vault/config/vault_config_substituted.hcl &
+else
+  export VAULT_EXTERNAL_PORT=8300
+  sed "s|\${VAULT_EXTERNAL_PORT}|$VAULT_EXTERNAL_PORT|g" /vault/config/vault_config.hcl > /vault/config/vault_config_substituted.hcl
+  vault server -dev -config=/vault/config/vault_config_substituted.hcl &
+fi
+VAULT_PID=$!  # Capture the process ID of the Vault server
+
+log "Waiting for Vault to be ready..."
+check_vault_status "200 OK|initialized"
--- docker/hashicorp_vault/scripts/vault_startup.sh
+++ docker/hashicorp_vault/scripts/vault_startup.sh
@@ -2,40 +2,13 @@
 
 . /opt/vault/common.sh
 
-terminate_vault() {
-  if [ -n "$VAULT_PID" ]; then
-    log "Terminating Vault server..."
-    kill $VAULT_PID
-    wait $VAULT_PID 2>/dev/null
-    log "Vault server terminated."
-  else
-    log "Vault server was not running."
-  fi
-}
-
-trap terminate_vault INT TERM EXIT
-
-# Start Vault server in the background
-log "Starting Vault server..."
-if [ "$SERVER_MODE" = "prod" ]; then
-  export VAULT_EXTERNAL_PORT=8200
-  sed "s|\${VAULT_EXTERNAL_PORT}|$VAULT_EXTERNAL_PORT|g" /vault/config/vault_config.hcl > /vault/config/vault_config_substituted.hcl
-  vault server -config=/vault/config/vault_config_substituted.hcl &
-else
-  export VAULT_EXTERNAL_PORT=8300
-  sed "s|\${VAULT_EXTERNAL_PORT}|$VAULT_EXTERNAL_PORT|g" /vault/config/vault_config.hcl > /vault/config/vault_config_substituted.hcl
-  vault server -dev -config=/vault/config/vault_config_substituted.hcl &
-fi
-VAULT_PID=$!  # Capture the process ID of the Vault server
-
 if [ -z "$ENVIRONMENT" ]; then
   log "ENVIRONMENT variable is not set. Please set it before running the script." "ERROR"
   exit 1
 fi
 
-log "Waiting for Vault to be ready..."
-check_vault_status "200 OK|initialized"
-log "Vault is ready."
+log "Starting Vault setup..."
+. /opt/vault/start_vault.sh
 
 log "Configuring Vault Environment..."
 sh /opt/vault/vault_configure.sh
