1839dc566c31bde0380e84b3bb84b51782bacd58
Muhammad Faisal <faisalazam_1616@hotmail.com>
2025-01-07 10:27:45 +1100

    Ensured cleanup of the contents of the "/vault/secrets" as the dir itself can't be deleted as it is mounted.

--- docker/vault_agent/scripts/agent_startup.sh
+++ docker/vault_agent/scripts/agent_startup.sh
@@ -2,28 +2,39 @@
 
 . /vault/scripts/common.sh
 
-rm -rf "/vault/secrets"
+SECRETS_DIR="/vault/secrets"
+
+if [ -e "$SECRETS_DIR" ]; then
+  log "Directory exists: $SECRETS_DIR.  Cleaning contents..."
+  rm -rf "${SECRETS_DIR:?}/"* || {
+    log "Failed to delete the contents of $SECRETS_DIR. Exiting." "ERROR"
+    exit 1
+  }
+  log "Successfully cleaned up $SECRETS_DIR."
+else
+  log "Directory does not exist: $SECRETS_DIR. Nothing to clean up."
+fi
 
 # Create required directories
-mkdir -p "/vault/secrets/config"
-mkdir -p "/vault/secrets/auth/ssh_keys"
-mkdir -p "/vault/secrets/auth/agent/$SSH_MANAGER_ROLE_NAME"
+mkdir -p "$SECRETS_DIR/config"
+mkdir -p "$SECRETS_DIR/auth/ssh_keys"
+mkdir -p "$SECRETS_DIR/auth/agent/$SSH_MANAGER_ROLE_NAME"
 
 # Set ownership and permissions for security
-chown -R vault:vault /vault/secrets
-chmod -R 770 /vault/secrets
+chown -R vault:vault "$SECRETS_DIR"
+chmod -R 770 "$SECRETS_DIR"
 
 # Combine all .hcl files in the /vault/config directory
-cat /vault/config/*.hcl > /vault/secrets/config/vault_agent_combined.hcl
+cat /vault/config/*.hcl > "$SECRETS_DIR/config/vault_agent_combined.hcl"
 
 # Substitute variables in HCL using sed
 sed -e "s|\${VAULT_ADDR}|$VAULT_ADDR|g" \
     -e "s|\${ENVIRONMENT}|$ENVIRONMENT|g" \
     -e "s|\${SSH_MANAGER_ROLE_NAME}|$SSH_MANAGER_ROLE_NAME|g" \
-    /vault/secrets/config/vault_agent_combined.hcl > /vault/secrets/config/vault_agent_resolved.hcl
+    "$SECRETS_DIR/config/vault_agent_combined.hcl" > "$SECRETS_DIR/config/vault_agent_resolved.hcl"
 
 # Start the Vault Agent
-vault agent -config=/vault/secrets/config/vault_agent_resolved.hcl &
+vault agent -config="$SECRETS_DIR/config/vault_agent_resolved.hcl" &
 AGENT_PID=$!  # Capture the process ID of the Vault Agent
 
 check_vault_status
