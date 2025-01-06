5b4cbff2d7460dd534a0d4bb1f69f9fc566c973a
Muhammad Faisal <faisalazam_1616@hotmail.com>
2025-01-06 15:13:59 +1100

    Improved the script to fetch ssh keys based on the environment.

--- docker/vault_agent/scripts/fetch_ssh_keys.sh
+++ docker/vault_agent/scripts/fetch_ssh_keys.sh
@@ -12,19 +12,22 @@ fi
 
 # Iterate over each machine and create templates dynamically
 for MACHINE in $MACHINES; do
-  MACHINE_PATH="secret/data/ssh_keys/${ENVIRONMENT}/${MACHINE}"
+  MACHINE_PATH="secret/ssh_keys/${ENVIRONMENT}/${MACHINE}"
+  SSH_KEYS_DIR="/vault/secrets/auth/ssh_keys/${ENVIRONMENT}/${MACHINE}"
+
+  mkdir -p "$SSH_KEYS_DIR"
 
   # Fetch id_rsa
   log "Fetching id_rsa for $MACHINE..."
-  vault kv get -field=id_rsa "$MACHINE_PATH" > "/vault/secrets/auth/ssh_keys/${ENVIRONMENT}/${MACHINE}/id_rsa"
+  vault kv get -field=id_rsa "$MACHINE_PATH" > "$SSH_KEYS_DIR/id_rsa"
 
   # Fetch id_rsa.pub
   log "Fetching id_rsa.pub for $MACHINE..."
-  vault kv get -field=id_rsa.pub "$MACHINE_PATH" > "/vault/secrets/auth/ssh_keys/${ENVIRONMENT}/${MACHINE}/id_rsa.pub"
+  vault kv get -field=id_rsa.pub "$MACHINE_PATH" > "$SSH_KEYS_DIR/id_rsa.pub"
 
   # Set permissions for the keys
-  chmod 600 "/vault/secrets/auth/ssh_keys/${ENVIRONMENT}/${MACHINE}/id_rsa"
-  chmod 644 "/vault/secrets/auth/ssh_keys/${ENVIRONMENT}/${MACHINE}/id_rsa.pub"
+  chmod 600 "$SSH_KEYS_DIR/id_rsa"
+  chmod 644 "$SSH_KEYS_DIR/id_rsa.pub"
 
   log "SSH keys fetched and stored for $MACHINE."
 done
