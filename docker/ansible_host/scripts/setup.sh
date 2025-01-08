988a2e0c586010cbdf65b4b9c152e9f0f346ba4e
Muhammad Faisal <faisalazam_1616@hotmail.com>
2025-01-08 15:06:00 +1100

    Explicitly adding the public key of linux_explicit_ssh_keys_host to avoid the use of keyscan. We should do this as we have the public key of linux_explicit_ssh_keys_host.

--- docker/ansible_host/scripts/setup.sh
+++ docker/ansible_host/scripts/setup.sh
@@ -25,15 +25,37 @@ if [ "${RESET_HOSTS_FILE}" = "true" ]; then
   # Path to known_hosts
   KNOWN_HOSTS_FILE="/root/.ssh/known_hosts"
 
-  # Remove old entry for linux hosts
+  # Remove old entries for specified hosts to avoid connection issues.
+  # For example, when the SSH server's host key changes (e.g., the server is rebuilt,
+  # or the key is regenerated), the existing key in the known_hosts file becomes outdated.
   if [ -f "$KNOWN_HOSTS_FILE" ]; then
     ssh-keygen -f "$KNOWN_HOSTS_FILE" -R "linux_ssh_pass_host"
     ssh-keygen -f "$KNOWN_HOSTS_FILE" -R "linux_ssh_keys_host"
   fi
 
-  # Add the current host key for linux hosts
+  # Adds the public key of the specified remote machines to the known_hosts file.
+  # ssh-keyscan is used to gather the public keys of the specified remote machine (or server)
+  # so that the supported keys get added to the known_hosts file.
+  # Run `ls -lart /etc/ssh` in remote machine to see the public keys.
   ssh-keyscan -H linux_ssh_pass_host >> "$KNOWN_HOSTS_FILE"
   ssh-keyscan -H linux_ssh_keys_host >> "$KNOWN_HOSTS_FILE"
+
+  # Configure linux_explicit_ssh_keys_host to use the externally generated keypair
+  hostname="linux_explicit_ssh_keys_host"  # Replace with the actual hostname or IP
+  key_file="/root/.ssh/linux_explicit_ssh_keys_host.pub"
+
+  # Extract key type and value
+  key_type=$(awk '{print $1}' "$key_file")
+  key_value=$(awk '{print $2}' "$key_file")
+
+  # Create a temporary file to hold the entry
+  temp_file=$(mktemp)
+  echo "$hostname $key_type $key_value" > "$temp_file"
+
+  # Hash the temporary file
+  ssh-keygen -H -f "$temp_file" 2>/dev/null
+  cat "$temp_file" >> "$KNOWN_HOSTS_FILE"
+  rm -f "$temp_file"
 fi
 
 # Install custom certificate if needed
