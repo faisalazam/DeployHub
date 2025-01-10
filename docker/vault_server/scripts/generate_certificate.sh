a13979081611afa997bdc8a34da46d293234530e
Muhammad Faisal <faisalazam_1616@hotmail.com>
2025-01-10 20:23:42 +1100

    Further organisation of script to make it reusable.

--- docker/vault_server/scripts/generate_certificate.sh
+++ docker/vault_server/scripts/generate_certificate.sh
@@ -5,19 +5,12 @@
 log "NOTE: THIS SCRIPT RUNS ON THE HOST..."
 
 BASE_DIR="certs/vaultCA"
-SERVER_DIR="$BASE_DIR/server"
 CONFIG_DIR="certs/config"
 DATABASE_DIR="$CONFIG_DIR/database"
 # TODO: Store the passphrase somewhere secure.
 PASSPHRASE="your_secure_passphrase"
 ROOT_CA_KEY="$BASE_DIR/private/cakey.pem"
 ROOT_CA_CERT="$BASE_DIR/cacert.pem"
-TEMP_KEY="$SERVER_DIR/temp/tempkey.pem"
-TEMP_REQ="$SERVER_DIR/temp/tempreq.pem"
-SERVER_KEY="$SERVER_DIR/server_key.pem"
-SERVER_CERT="$SERVER_DIR/server_crt.pem"
-FULL_CHAIN="$SERVER_DIR/full_chain.pem"
-
 RSA_KEY_SIZE=4096
 CERT_EXPIRY_DAYS=1825
 
@@ -37,6 +30,11 @@ create_dirs_and_files() {
 }
 
 generate_root_certificate() {
+  if [ -f "$ROOT_CA_CERT" ] && [ -f "$ROOT_CA_KEY" ]; then
+    log "Root certificate already exists. Skipping generation process." "INFO"
+    return
+  fi
+
   log "Generate the root certificate"
   if ! openssl req -x509 -newkey rsa:$RSA_KEY_SIZE \
       -out "$ROOT_CA_CERT" -outform PEM -days $CERT_EXPIRY_DAYS \
@@ -50,20 +48,30 @@ generate_root_certificate() {
 }
 
 generate_key_and_request() {
-  log "Generate the key and request using localhost configuration"
+  CERT_TYPE=$1
+  SERVER_DIR=$2
+  CONFIG_FILE=$3
+  TEMP_KEY="$SERVER_DIR/temp/tempkey.pem"
+  TEMP_REQ="$SERVER_DIR/temp/tempreq.pem"
+
+  log "Generate the key and request for $CERT_TYPE using $CONFIG_FILE"
   if ! openssl req -newkey rsa:$RSA_KEY_SIZE \
       -keyout "$TEMP_KEY" -keyform PEM \
       -out "$TEMP_REQ" -outform PEM \
       -passout pass:$PASSPHRASE \
-      -config "$CONFIG_DIR/localhost.cnf" -quiet; then
-    log "Failed to generate temporary key and certificate request" "ERROR"
+      -config "$CONFIG_FILE" -quiet; then
+    log "Failed to generate temporary key and certificate request for $CERT_TYPE" "ERROR"
     exit 1
   fi
-  log "Temporary key and certificate request generated successfully"
+  log "$CERT_TYPE temporary key and certificate request generated successfully"
 }
 
 extract_private_key() {
-  log "Extract the private key from the temporary key file"
+  SERVER_DIR=$1
+  TEMP_KEY="$SERVER_DIR/temp/tempkey.pem"
+  SERVER_KEY="$SERVER_DIR/server_key.pem"
+
+  log "Extract the private key for $SERVER_DIR"
   if ! openssl rsa -in "$TEMP_KEY" -out "$SERVER_KEY" \
       -passin pass:$PASSPHRASE; then
     log "Failed to extract the private key" "ERROR"
@@ -73,47 +81,66 @@ extract_private_key() {
 }
 
 sign_certificate_with_root_ca() {
-  log "Sign the certificate with the root CA configuration"
+  SERVER_DIR=$1
+  TEMP_REQ="$SERVER_DIR/temp/tempreq.pem"
+  SERVER_CERT="$SERVER_DIR/server_crt.pem"
+
+  log "Sign the certificate for $SERVER_DIR with root CA"
   if ! openssl ca -in "$TEMP_REQ" \
       -out "$SERVER_CERT" \
       -config "$CONFIG_DIR/caconfig.cnf" \
       -batch \
       -passin pass:$PASSPHRASE -quiet; then
-    log "Failed to sign the certificate with root CA" "ERROR"
+    log "Failed to sign the certificate for $SERVER_DIR with root CA" "ERROR"
     exit 1
   fi
-  log "Certificate signed successfully"
+  log "$SERVER_DIR certificate signed successfully"
 }
 
 combine_certificates_into_full_chain() {
-  log "Combine server certificate and CA certificate into the full chain"
+  SERVER_DIR=$1
+  SERVER_CERT="$SERVER_DIR/server_crt.pem"
+  FULL_CHAIN="$SERVER_DIR/full_chain.pem"
+
+  log "Combine server certificate and CA certificate into the full chain for $SERVER_DIR"
   if ! cat "$SERVER_CERT" "$ROOT_CA_CERT" > "$FULL_CHAIN"; then
-    log "Failed to combine server certificate and CA certificate" "ERROR"
+    log "Failed to combine server certificate and CA certificate for $SERVER_DIR" "ERROR"
     exit 1
   fi
-  log "Full chain certificate created successfully"
+  log "$SERVER_DIR full chain certificate created successfully"
 }
 
 clean_temp_files() {
-  log "Cleaning up temporary files"
+  SERVER_DIR=$1
+
+  log "Cleaning up temporary files for $SERVER_DIR"
   if ! rm -rf "$SERVER_DIR/temp"; then
-    log "Failed to remove temp directory" "ERROR"
+    log "Failed to remove temp directory for $SERVER_DIR" "ERROR"
     exit 1
   fi
-  log "Temporary directory removed successfully"
+  log "Temporary directory for $SERVER_DIR removed successfully"
 }
 
-if [ -f "$SERVER_CERT" ]; then
-  log "Certificate already exists. Skipping generation and signing process." "INFO"
-  exit 0
-fi
+# The main process for both server and agent
+generate_certificate() {
+  CERT_TYPE=$1
+  SERVER_DIR="$BASE_DIR/$CERT_TYPE"
+  CONFIG_FILE="$CONFIG_DIR/$CERT_TYPE.cnf"
+
+  if [ -f "$SERVER_DIR/server_crt.pem" ]; then
+    log "$CERT_TYPE certificate already exists. Skipping generation and signing process." "INFO"
+    return
+  fi
+
+  generate_key_and_request "$CERT_TYPE" "$SERVER_DIR" "$CONFIG_FILE"
+  extract_private_key "$SERVER_DIR"
+  sign_certificate_with_root_ca "$SERVER_DIR"
+  combine_certificates_into_full_chain "$SERVER_DIR"
+  clean_temp_files "$SERVER_DIR"
+
+  log "$CERT_TYPE certificate generation and signing process completed successfully"
+}
 
 create_dirs_and_files
 generate_root_certificate
-generate_key_and_request
-extract_private_key
-sign_certificate_with_root_ca
-combine_certificates_into_full_chain
-clean_temp_files
-
-log "Certificate generation and signing process completed successfully"
+generate_certificate "server"
